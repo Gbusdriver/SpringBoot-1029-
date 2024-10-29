4748bb4ad28a36462943c7d0fbcf82ec98aeabc9
unknown <usad63mbn@naver.com>
2024-10-29 17:26:17 +0900

    first commit

---
+++ src/main/java/com/SpringBootCustomerApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomerApplication.class, args);
+	}
+
+}
